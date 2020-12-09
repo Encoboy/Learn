2a0f41e8aac1563a32226f338593452191d40e33
Encoboy <443080804@qq.com>
2020-12-09 15:40:45 +0800

    update ..

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [active, setActive] = useState(0);
   return (
     <div>
-      {/* <div>
+      <div>
         <div onClick={() => setActive(0)}>Active</div>
         <div onClick={() => setActive(1)}>Gift</div>
       </div>
@@ -19,7 +19,7 @@ function App() {
         <Gift active={active} />
       </div>
       <Index />
-      <Call /> */}
+      <Call />
     </div>
   );
 }
